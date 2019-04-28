f15095922e1f539383a2a9ee1a5cecf4032f5795
Alena Bukilic <alena.bukilic@florencehc.com>
2019-04-28 18:47:40 +0200

    initial commit

---
+++ build/precache-manifest.838f69269ed7c08db87303c9e07fc1c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526ae267e0aa3a6d0dbba1a10b0d290c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ff439575d5e32a861eb0",
+    "url": "/static/css/main.c173d12c.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "ff439575d5e32a861eb0",
+    "url": "/static/js/main.47d1dd5e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
