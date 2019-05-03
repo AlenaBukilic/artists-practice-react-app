aacd2c13063a263e5fd5819a9a9cd3722d2612b5
Alena Bukilic <bukilicalena@gmail.com>
2019-05-03 21:03:34 +0200

    add redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
+
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            artists: [],
-            searchfield: ''
+            artists: []
         }
     }
 
@@ -21,27 +36,26 @@ class App extends Component {
         });
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value});
-    }
-
     render() {
-        const { artists, searchfield } = this.state;
+        const { artists } = this.state;
+        const { searchField, onSearchChange } = this.props;
         const filteredArtists = artists.filter(artist => {
-            return artist.author.toLowerCase().includes(searchfield.toLowerCase());
+            return artist.author.toLowerCase().includes(searchField.toLowerCase());
         });
         return !artists.length ?
         <h1>Loading...</h1> :
         (
             <div className='tc'>
                 <h1 className='f1'>Artists</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
-                    <CardList artists={filteredArtists}/>
+                    <ErrorBoundary>
+                        <CardList artists={filteredArtists}/>
+                    </ErrorBoundary>
                 </Scroll>
             </div>
         );
     }
 };
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {  createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { searchArtists } from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(searchArtists);
 
+ReactDOM.render(
+                <Provider store={store}>
+                    <App />
+                </Provider>, document.getElementById('root'));
 serviceWorker.unregister();
